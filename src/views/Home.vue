14a806905297e585e093f52c4d187066dcda2b05
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-27 13:24:49 +0300

    *Added blogCard components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,15 +6,29 @@
       v-for="(post, index) in sampleBlogPost"
       :key="index"
     />
+    <div class="blog-card-wrap">
+      <div class="container">
+      <h3>View Recent Blogs</h3>
+      <div class="blog-cards">
+        <BlogCard
+          :post="post"
+          v-for="(post, index) in blogPostsCards"
+          :key="index"
+        />
+      </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BlogCard from "@/components/BlogCard.vue";
 import BlogPost from "@/components/BlogPost.vue";
 export default {
   name: "HomePage",
   components: {
     BlogPost,
+    BlogCard,
   },
   data() {
     return {
@@ -37,7 +51,38 @@ export default {
           blogCoverPhoto: "coding",
         },
       ],
+      blogPostsCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "April 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "April 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "April 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "April 4, 2021",
+        },
+      ],
     };
   },
 };
 </script>
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
